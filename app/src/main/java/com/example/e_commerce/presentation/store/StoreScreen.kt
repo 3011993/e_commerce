6233c7be738d4089940ac0c19415ef53d027dc55
Ahmed Mosad <ahmedmooth@gmail.com>
2024-09-19 17:23:03 +0300

    change the look of refresh button

--- app/src/main/java/com/example/e_commerce/presentation/store/StoreScreen.kt
+++ app/src/main/java/com/example/e_commerce/presentation/store/StoreScreen.kt
@@ -6,15 +6,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -27,6 +27,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -93,7 +95,6 @@ fun StoreContent(
         previousConnection = connection
     }
 
-
     Box(modifier = modifier.fillMaxSize()) {
         when (state) {
             is ScreenState.Error -> {
@@ -101,11 +102,6 @@ fun StoreContent(
                     products = state.data ?: emptyList(),
                     onProductClick = onProductClick,
                     onCartButtonClicked = onCartButtonClicked,
-                    getAllProducts = {
-                        getAllProducts()
-                        showRefreshButton = false
-                    },
-                    showRefreshButton = showRefreshButton,
                     isConnected = isConnected,
                     searchBar = {
                     }
@@ -123,12 +119,8 @@ fun StoreContent(
                     products = displayedProducts,
                     onProductClick = onProductClick,
                     onCartButtonClicked = onCartButtonClicked,
-                    getAllProducts = {
-                        getAllProducts()
-                        showRefreshButton = false
-                    },
                     isConnected = isConnected,
-                    showRefreshButton = showRefreshButton, searchBar = {
+                    searchBar = {
                         SearchBar(searchText = searchText,
                             onSearchTextChange = { newValue ->
                                 searchText = newValue
@@ -143,6 +135,20 @@ fun StoreContent(
                 )
             }
         }
+        if (showRefreshButton) {
+            IconButton(
+                onClick = {
+                    getAllProducts()
+                    showRefreshButton = false
+                },
+                modifier = modifier
+                    .clip(CircleShape)
+                    .background(Color.White)
+                    .align(Alignment.TopCenter),
+            ) {
+                Icon(imageVector = Icons.Default.Refresh, contentDescription = null)
+            }
+        }
     }
 }
 
@@ -152,9 +158,7 @@ fun ProductsLazyVerticalGrid(
     onProductClick: (ProductModel) -> Unit,
     onCartButtonClicked: (ProductModel) -> Unit,
     isConnected: Boolean,
-    getAllProducts: () -> Unit,
     searchBar : @Composable () -> Unit ,
-    showRefreshButton: Boolean,
     modifier: Modifier = Modifier,
 ) {
     var showSnackBar by remember { mutableStateOf(true) }
@@ -172,16 +176,6 @@ fun ProductsLazyVerticalGrid(
         }
 
         item(span = { GridItemSpan(2) }) {
-            if (showRefreshButton) {
-                Button(
-                    onClick = getAllProducts,
-                    modifier = modifier
-                        .background(MaterialTheme.colorScheme.primary)
-                        .wrapContentSize(),
-                ) {
-                    Icon(imageVector = Icons.Default.Refresh, contentDescription = null)
-                }
-            }
             if (showSnackBar && !isConnected) {
                 SnackBarManager.showMessage(AppText.offline_message)
                 showSnackBar = false
