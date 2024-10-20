6552b142d91fe3afbe3b00e8c5ab1cce71c32e12
Ahmed Mosad <ahmedmooth@gmail.com>
2024-10-20 06:57:47 +0300

    adjust button at bottom in order confirmation

--- app/src/main/java/com/example/e_commerce/presentation/order_confirmation/OrderConfirmationScreen.kt
+++ app/src/main/java/com/example/e_commerce/presentation/order_confirmation/OrderConfirmationScreen.kt
@@ -1,11 +1,9 @@
 package com.example.e_commerce.presentation.order_confirmation
 
-import androidx.compose.foundation.gestures.rememberScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,7 +14,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Scaffold
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -68,17 +65,25 @@ fun OrderConfirmationContent(
     onNavigationBack: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
-        CommerceToolBar(
-            title = AppText.order_confirmation_bar,
-            navigationIcon = AppIcon.back,
-            onNavigationBack = onNavigationBack
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(
+            modifier = modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CommerceToolBar(
+                title = AppText.order_confirmation_bar,
+                navigationIcon = AppIcon.back,
+                onNavigationBack = onNavigationBack
+            )
+            OrderSummarySection(carts)
+            AddressSection(onAddressClicked)
+            PaymentSection(onPaymentClicked)
+        }
+        CommerceWideButton(
+            AppText.place_order_button,
+            action = {},
+            modifier = modifier.align(Alignment.BottomCenter)
         )
-        OrderSummarySection(carts)
-        AddressSection(onAddressClicked)
-        PaymentSection(onPaymentClicked)
-        Spacer(modifier.weight(1f))
-        CommerceWideButton(AppText.place_order_button, action = {})
     }
 }
 
