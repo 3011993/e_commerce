5479d4bb75bc58ce3f9a789167b41439800bed7c
Ahmed Mosad <ahmedmooth@gmail.com>
2025-06-27 19:13:42 +0300

    add render backend for all projects

--- app/src/main/java/com/example/e_commerce/common/Constants.kt
+++ app/src/main/java/com/example/e_commerce/common/Constants.kt
@@ -6,6 +6,6 @@ object Constants {
     const val GET_SINGLE_PRODUCT = "products/{id}"
     const val GET_ALL_CATEGORIES = "products/categories"
     const val GET_PRODUCTS_BY_CATEGORY = "products/category/{category}"
-    const val BACKEND_URL = "http://10.0.2.2:4243"
+    const val BACKEND_URL = "https://ecommerce-stripe-backend.onrender.com"
 
 }
