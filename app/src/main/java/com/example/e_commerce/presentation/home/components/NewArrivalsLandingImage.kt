ae1943851787086a24c75655da4b788104d8d65e
Ahmed Mosad <ahmedmooth@gmail.com>
2025-06-30 07:09:02 +0300

    adjust padding for home bar

--- app/src/main/java/com/example/e_commerce/presentation/home/components/NewArrivalsLandingImage.kt
+++ app/src/main/java/com/example/e_commerce/presentation/home/components/NewArrivalsLandingImage.kt
@@ -3,6 +3,7 @@ package com.example.e_commerce.presentation.home.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -15,7 +16,7 @@ import com.example.e_commerce.ui.theme.E_commerceTheme
 @Composable
 fun NewArrivalsLandingImage(modifier: Modifier = Modifier) {
     Image(painter = painterResource(R.drawable.new_arrivals), contentDescription = null,
-        modifier = modifier.fillMaxWidth().heightIn(min =160.dp, max= 210.dp), contentScale = ContentScale.FillWidth)
+        modifier = modifier.fillMaxWidth().heightIn(min =160.dp, max= 210.dp).padding(top = 8.dp), contentScale = ContentScale.FillWidth)
 }
 @Preview(showBackground = true)
 @Composable
